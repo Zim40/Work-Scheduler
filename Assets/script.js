58a87ecaac03e1f96fb2a61c2d92802f81668ee9
User <michaelm810129@gmail.com>
2023-01-15 17:38:26 +1030

    Added notes/Added readme text/organized files into assets

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,14 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-
 $(function () {
 
   // Displays time  below header
   // ============================
   window.onload = titleClock;
-
-
   function titleClock() {
-
     var timeTitle = document.querySelector("#currentDay");
     timeTitle.innerText = dayjs().format("MMMM D YYYY - HH:mm a");
     setInterval(titleClock, 60000);
-
-
   }
+
   function handleSave() {
     let buttons = document.querySelectorAll(".saveBtn");
     for (let i = 0; i < buttons.length; i++) {
@@ -31,6 +22,7 @@ $(function () {
   }
   handleSave();
 
+  // Calls the saved user input and replaces the contents in the respective containers they were written in.
   window.addEventListener("load", function () {
     var textInput = document.querySelectorAll(".description");
     for (var i=0; i < textInput.length; i++) {
@@ -68,35 +60,5 @@ $(function () {
   }  
   bgChange();
 
-  
- 
-
- 
-
-  
-  
-
-
-
-
-
-  //   //   // TODO: Add a listener for click events on the save button. This code should
-  //   //   // use the id in the containing time-block as a key to save the user input in
-  //   //   // local storage. HINT: What does `this` reference in the click listener
-  //   //   // function? How can DOM traversal be used to get the "hour-x" id of the
-  //   //   // time-block containing the button that was clicked? How might the id be
-  //   //   // useful when saving the description in local storage?
-  //   //   //
-  //   //   // TODO: Add code to apply the past, present, or future class to each time
-  //   //   // block by comparing the id to the current hour. HINTS: How can the id
-  //   //   // attribute of each time-block be used to conditionally add or remove the
-  //   //   // past, present, and future classes? How can Day.js be used to get the
-  //   //   // current hour in 24-hour time?
-  //   //   //
-  //   //   // TODO: Add code to get any user input that was saved in localStorage and set
-  //   //   // the values of the corresponding textarea elements. HINT: How can the id
-  //   //   // attribute of each time-block be used to do this?
-  //   //   //
-  //   //   // TODO: Add code to display the current date in the header of the page.
 })
 
---
+++ readme.md
@@ -0,0 +1,38 @@
+# Work Scheduler
+## About
+This Work Scheduler utilizes the use of Javascript and  Api`s to call upon data that is stored through the use of 'save buttons' and presenting the saved data which is persistent after the page is refreshed.
+
+Using dayjs and formatting correctly i was able to present the date and time within a function that updates itself every minute.
+``` js
+// Displays time  below header
+  // ============================
+  window.onload = titleClock;
+  function titleClock() {
+    var timeTitle = document.querySelector("#currentDay");
+    timeTitle.innerText = dayjs().format("MMMM D YYYY - HH:mm a");
+    setInterval(titleClock, 60000);
+  }
+```
+
+# Usage
+Once the user has typed something into the text area, once the save button is click the data is then stored in localStorage, being called upon as the page loads/refreshes gives the user the ability to keep track of tasks ahead and also in the past. 
+``` js
+// Calls the saved user input and replaces the contents in the respective containers they were written in.
+  window.addEventListener("load", function () {
+    var textInput = document.querySelectorAll(".description");
+    for (var i=0; i < textInput.length; i++) {
+      var savedText = localStorage.getItem("Key" + i);
+      if (savedText) {
+        textInput[i].value = savedText;
+      }
+    }
+  }); 
+```
+# Licence 
+MIT Licence - refer to GitHub.
+
+## Links
+
+Live Page -
+ 
+!["WorkScheduler"](./Assets/Project.png) 

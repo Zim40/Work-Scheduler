3c860c711102fcfc0d56d3456284ee60022afd2f
User <michaelm810129@gmail.com>
2023-01-15 17:13:57 +1030

    feature/Added dynamic bgColors dependant on time/Added comments

--- Assets/script.js
+++ Assets/script.js
@@ -5,7 +5,7 @@
 
 $(function () {
 
-  // Displays time on below header
+  // Displays time  below header
   // ============================
   window.onload = titleClock;
 
@@ -33,13 +33,46 @@ $(function () {
 
   window.addEventListener("load", function () {
     var textInput = document.querySelectorAll(".description");
-    for (var i=0; i<textInput.length; i++) {
+    for (var i=0; i < textInput.length; i++) {
       var savedText = localStorage.getItem("Key" + i);
       if (savedText) {
         textInput[i].value = savedText;
       }
     }
   });
+
+  // Added classes to each div depending if dayjs is in the past present or future=================================
+  setInterval(bgChange, 3600000);
+  function bgChange () {
+    var now = dayjs();
+    var items = document.querySelectorAll(".time-block");
+    console.log("bgchange");
+
+    // Compare the current hour with dayjs()======================
+    items.forEach(item => {
+      if (item.dataset.time === now.format('HH')) {
+        item.classList.add('present');
+        item.classList.remove('future');
+        item.classList.remove('past');
+      }else if(item.dataset.time > now.format('HH')) {
+        item.classList.add('future');
+        item.classList.remove('present');
+        item.classList.remove('past');
+      }else{
+        item.classList.add('past');
+        item.classList.remove('future');
+        item.classList.remove('present');
+      }
+    
+    }); 
+  }  
+  bgChange();
+
+  
+ 
+
+ 
+
   
   
 
